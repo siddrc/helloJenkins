a52cd0e081aa0b1479de505144bff5d1b27854c6
Siddharth RoyChoudhury <debu2in@gmail.com>
2016-01-07 20:10:10 +0530

    test deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins test 8');
+  res.send('hello jenkins test 9');
 });
  
 app.listen(process.env.PORT || 5000);
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins test 8', done);
+    request(app).get('/').expect('hello jenkins test 9', done);
   });
 });
